48928aa556eb548fd98361b7f3018263039d4411
Nifras <nifrasismail@gmail.com>
2015-05-11 14:36:36 +0530

    sorting implemented

--- src/Insertion_Sorting.java
+++ src/Insertion_Sorting.java
@@ -1,6 +1,38 @@
 
 public class Insertion_Sorting {
+	
+	private int[] A;
+	
+	public void read(int[] A){
+		this.A = A;
+	}
+	
+	public int[] sort(){
+		for (int i = 1; i <= A.length-1; i++) {
+			int element = A[i];
+			int j = i;
+			while (j>0 && (A[j-1] > element)) {
+				A[j] = A[j-1];
+				j--;
+			}
+			A[j] = element;
+		}
+		return A;
+	}
+	
+	public void print(){
+		for (int i = 0; i < A.length; i++) {
+			System.out.print(A[i] + " ");
+		}
+	}
+	
+	
 	public static void main(String[] args){
-		System.out.println("Hello World!");
+		int[] A = {23,42,4,16,8,15};
+		Insertion_Sorting I = new Insertion_Sorting();
+		
+		I.read(A);
+		I.sort();
+		I.print();
 	}
 }
